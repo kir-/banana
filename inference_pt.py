48c42b3b075dfae297d9e34c2a61fa38220a2e14
graysonl <graysonl@sfu.ca>
2023-11-23 11:17:16 -0800

    profile every 1000 entry

--- inference_pt.py
+++ inference_pt.py
@@ -41,42 +41,56 @@ def inference():
     model.load_state_dict(torch.load("data/banana_final.pt"))
     model = model.to(device)
     model.eval()
+    profiler_log_file = "profiler_results.txt"
+    with open(profiler_log_file, "w") as profiler_log:
+        with open(args.out_file, "a") as f:
+            # Warning this runs on per tuple of a pt file and therefore is very slow
+            # TODO Generalize dataset to run on pocket or multiple pockets
+            for i, entry in enumerate(tqdm(combined_data, desc="Inference on protein + ligand", unit="ligand")):
+                pocket_name, ligand_name = entry
+                pocket = os.path.join(data_path, pocket_name)
+                ligand = os.path.join(data_path, ligand_name)
+                mol = Chem.SDMolSupplier(ligand)[0]
+                smi = Chem.MolToSmiles(mol)
+                if i % 1000 == 0:
+                    with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True, use_cuda=not args.no_gpu) as prof:
+                        pocket_name, ligand_name = entry
+                        try:
+                            dataset = InferenceDataset(cfg, smi, pocket)
+                        except:
+                            logging.warning("{pocket_name}: pdb incorrect format")
+                        dataloader = DataLoader(dataset, batch_size=args.batch_size,
+                                                num_workers=args.num_workers, pin_memory=True,
+                                                shuffle=False)
 
-    with open(args.out_file, "a") as f:
-        # Warning this runs on per tuple of a pt file and therefore is very slow
-        # TODO Generalize dataset to run on pocket or multiple pockets
-        for entry in tqdm(combined_data, desc="Inference on protein + ligand", unit="ligand"):
-            pocket_name, ligand_name = entry
-            pocket = os.path.join(data_path, pocket_name)
-            ligand = os.path.join(data_path, ligand_name)
-            mol = Chem.SDMolSupplier(ligand)[0]
-            smi = Chem.MolToSmiles(mol)
-            try:
-                dataset = InferenceDataset(cfg, smi, pocket)
-            except:
-                logging.warning("{pocket_name}: pdb incorrect format")
-            dataloader = DataLoader(dataset, batch_size=args.batch_size,
-                                    num_workers=args.num_workers, pin_memory=True,
-                                    shuffle=False)
-
-            ligand_txt = ligand_name.replace('/','-')[:-3] + 'txt'
-            
-            for batch in tqdm(dataloader):
-                batch = batch.to(device)
-                with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], 
-                             record_shapes=True, 
-                             profile_memory=True, 
-                             use_cuda=not args.no_gpu) as prof:
-                    output = model(batch).cpu().numpy()
-                
-                print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))                
-                # output = model(batch).cpu().numpy()
-                for x, logits in zip(batch, output):
-                    prob = torch.sigmoid(logits)
-                    if not x.is_active:
-                        out = -100
-                    f.write(f"{ligand_txt},{out}\n")
-           
+                        ligand_txt = ligand_name.replace('/','-')[:-3] + 'txt'
+                        
+                        for batch in tqdm(dataloader):
+                            batch = batch.to(device)
+                            output = model(batch)
+                                        
+                            # output = model(batch).cpu().numpy()
+                            for x, logits in zip(batch, output):
+                                prob = torch.sigmoid(logits).cpu().numpy()
+                                if not x.is_active:
+                                    out = -100
+                                f.write(f"{ligand_txt},{prob}\n")
+                    profiler_log.write(f"Profiling results for {entry}:\n")
+                    profiler_log.write(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+                    profiler_log.write("\n\n")  # Add some space between entries
+                else:
+                    ligand_txt = ligand_name.replace('/','-')[:-3] + 'txt'
+                        
+                    for batch in tqdm(dataloader):
+                        batch = batch.to(device)
+                        output = model(batch)
+                                        
+                        # output = model(batch).cpu().numpy()
+                        for x, logits in zip(batch, output):
+                            prob = torch.sigmoid(logits).cpu().numpy()
+                            if not x.is_active:
+                                out = -100
+                            f.write(f"{ligand_txt},{prob}\n")
 
 if __name__ == "__main__":
     with torch.no_grad():
